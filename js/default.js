00a6815da731a6db04e8b91533035dfd210f172b
bollu <siddu.druid@gmail.com>
2016-02-25 18:44:39 +0530

    fixed macOS quit - now quits with cmd+Q

--- js/default.js
+++ js/default.js
@@ -285,6 +285,18 @@ $(document).ready(function() {
         terminal.enable();
     });
     internet.loadResources();
+
+    //workaround for mac OS for quit to work
+    var quitKeys = {
+        key: "Ctrl+Q"
+    };
+
+    var shortcutQuit = new gui.Shortcut(quitKeys);
+    gui.App.registerGlobalHotKey(shortcutQuit);
+    shortcutQuit.on('active', function() {
+        // quit your app
+        win.close();
+    });
 });
 
 $(document).on('keydown', function(e) {
